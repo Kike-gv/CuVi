effa376385cc663ce5a17c34564a384f8fe36f82
Enrique Garcia <enrique.garcia@raona.com>
2019-12-12 13:01:02 +0100

    filtering comapny

--- app_Cuvi/screens/OfferCompanyScreen/OfferCompanyScreen.js
+++ app_Cuvi/screens/OfferCompanyScreen/OfferCompanyScreen.js
@@ -5,13 +5,18 @@ import { connect, useSelector, useDispatch } from 'react-redux';
 import { setUser } from '../../redux/actions/userActions';
 
 import { logout } from '../../services/auth';
+import { registerAuthObserver } from '../../services/auth';
+import { getItem, getAll } from '../../services/database';
 
 import CuviButton from '../../components/CuviButton';
 
-const OfferCompanyScreen = ({navigation}) => {
+const OfferCompanyScreen = ({ navigation }) => {
     const state = useSelector(state => state);
-    console.log("TCL: AuthLoadingScreen -> state", state)
     const dispatch = useDispatch();
+
+    const [filterWord, setFilterWord] = useState('');
+    const [chosenUsers, setChosenUsers] = useState('');
+    console.log("TCL: OfferCompanyScreen -> chosenUsers", chosenUsers)
 
     const signOutAsync = async () => {
         dispatch(setUser(null));
@@ -20,10 +25,30 @@ const OfferCompanyScreen = ({navigation}) => {
         navigation.navigate('Auth');
     };
 
+    const searchResults = async () => {
+        const users = await getAll('Usuarios');
+        const selectedUsers = [];
+        users.map((user) => {
+            if (user.cvSkill === filterWord) {
+                selectedUsers.push(user);
+            }
+        })
+        setChosenUsers(selectedUsers);
+    };
+
+
+
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Hola Business.</Text>
             <CuviButton name='Log out' icon='md-exit' textColor='#383838' bgColor='rgba(199, 128, 33, 0.25)' clickedEvent={signOutAsync} />
+            <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center' }}>
+                <TextInput placeholder='busca por talento' value={filterWord} onChangeText={(text) => { setFilterWord(text) }} />
+                <TouchableOpacity onPress={() => searchResults()}><Text>busca</Text></TouchableOpacity>
+            </View>
+
+            <View>
+                {chosenUsers !== '' && chosenUsers.map((userFiltered)=><TouchableOpacity><Text>{userFiltered.cvName}</Text></TouchableOpacity>)}
+            </View>
         </View>
     );
 }
